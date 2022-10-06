282154221c153191cf3289ec3abe29d16016f331
vratant11 <vratant2012061@akgec.ac.in>
2022-10-06 15:03:23 +0530

    Skills added in swiper

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import "./slider.css";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { EffectFlip, Pagination, Navigation } from "swiper";
+
 // Import Swiper styles
 import "swiper/css";
+import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
-import "swiper/css/effect-flip";
-// import "swiper/css/navigation";
+
+import "./slider.css";
+
+// import required modules
+import { EffectCoverflow, Pagination } from "swiper";
 const slider = () => {
   return (
     <>
@@ -23,41 +26,46 @@ const slider = () => {
         </div>
       </div> */}
       <Swiper
-        effect={"flip"}
+        effect={"coverflow"}
         grabCursor={true}
-        pagination={true}
-        autoplay={{
-          delay:2500,
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
         }}
-        navigation={true}
-        modules={[EffectFlip, Pagination, Navigation]}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
         className="mySwiper"
       >
         <SwiperSlide>
-        <img src="/assets/bootstrap.png" alt="error"/>
+          <img src="/assets/bootstrap.png" alt="Error" />
         </SwiperSlide>
         <SwiperSlide>
-        <img src="/assets/cimage.jpg" alt="error"/>
+          <img src="/assets/cimage.jpg" alt="Error" />
         </SwiperSlide>
         <SwiperSlide>
-        <img src="/assets/c++image.jpg" alt="error"/>
+          <img src="/assets/c++image.jpg" alt="Error" />
         </SwiperSlide>
         <SwiperSlide>
-        <img src="/assets/cssimage.png" alt="error"/>
+          <img src="/assets/cssimage.png" alt="Error" />
         </SwiperSlide>
         <SwiperSlide>
-        <img src="/assets/html5image.png" alt="error"/>
+          <img src="/assets/html5image.png" alt="Error" />
         </SwiperSlide>
         <SwiperSlide>
-        <img src="/assets/javascript.png" alt="error"/>
+          <img src="/assets/javascript.png" alt="Error" />
         </SwiperSlide>
         <SwiperSlide>
-        <img src="/assets/git.png" alt="error"/>
+          <img src="/assets/git.png" alt="Error" />
         </SwiperSlide>
         <SwiperSlide>
-        <img src="/assets/react3.jpg" alt="error"/>
+          <img src="/assets/react3.jpg" alt="Error" />
         </SwiperSlide>
-      </Swiper>
+      </Swiper>      
     </>
   )
 }
